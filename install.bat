6b32a617a0e614c51611be9b9e3e8d0937c29094
seanbf <83425701+seanbf@users.noreply.github.com>
2021-10-13 13:38:20 +0100

    Add files via upload

--- install.bat
+++ install.bat
@@ -28,13 +28,41 @@ goto start
 :install_full
 ECHO Installing Python 3.7.4, Paths, Dependancies and Launching Torque Accuracy Tool
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}"
+
+ECHO Generating Desktop Shortcut and Icon
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\Torque Accuracy Tool.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath ="%comspec%" >> %SCRIPT%
+echo oLink.Arguments = " /c ""%ThisScriptsDirectory%torque_accuracy_tool.bat""" >> %SCRIPT%
+echo oLink.IconLocation = "%ThisScriptsDirectory%\icon\accuracy.ico" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
 goto end
 
 :install_dep
 ECHO Installing Dependancies and Launching Torque Accuracy Tool
+
+ECHO Generating Desktop Shortcut and Icon
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\Torque Accuracy Tool.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath ="%comspec%" >> %SCRIPT%
+echo oLink.Arguments = " /c ""%ThisScriptsDirectory%torque_accuracy_tool.bat""" >> %SCRIPT%
+echo oLink.IconLocation = "%ThisScriptsDirectory%\icon\accuracy.ico" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
 py -3.7 -m pip install -r %ThisScriptsDirectory%\program\install\requirements.txt
 py -3.7 -m streamlit run %ThisScriptsDirectory%\program\torque_accuracy_tool.py
 goto end
+
+echo off
 
 :end
 pause
